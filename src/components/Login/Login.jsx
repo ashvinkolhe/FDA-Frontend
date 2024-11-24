e74cf6a9b7e26e96130da79898ce17f9b6e05320
Ashwin Kolhe <ashwinkolhe09@gmail.com>
2024-11-25 01:20:04 +0530

    updated code

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,33 +7,67 @@ import "./Login.css";
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <div className="login-content">
-        <div className="login-form">
-          <h1 className="login-title">Order <span className="highlight">UK</span></h1>
-          <h2>Welcome Back 👋</h2>
-          <p>Today is a new day. It's your day. You shape it. <br /> Sign in to start ordering.</p>
-          <form>
-            <div className="input-group">
-              <label htmlFor="email" className="input-label">Email</label>
-              <input id="email" type="email" placeholder="Example@email.com" className="input-field" />
-            </div>
-            <div className="input-group">
-              <label htmlFor="password" className="input-label">Password</label>
-              <input id="password" type="password" placeholder="At least 8 characters" className="input-field" />
-            </div>
-            <a href="#" className="forgot-password">Forgot Password?</a>
-            <button type="submit" className="login-button">Sign in</button>
-          </form>
-          <p className="signup-text">
-            Don't have an account? <a href="#" className="signup-link">Sign up</a>
-          </p>
-        </div>
-        <div className="login-image">
-          <img src={loginBanner} alt="Login Banner" />
-        </div>
+<div className="login-container">
+  <div className="login-content">
+    {/* Left Container */}
+    <div className="login-left">
+      <div className="login-form">
+        <h1 className="login-title">
+          Order <span className="highlight">UK</span>
+        </h1>
+        <h2>Welcome Back 👋</h2>
+        <p>
+          Today is a new day. It's your day. You shape it. <br /> Sign in to
+          start ordering.
+        </p>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email" className="input-label">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Example@email.com"
+              className="input-field"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password" className="input-label">
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="At least 8 characters"
+              className="input-field"
+            />
+          </div>
+          <a href="#" className="forgot-password">
+            Forgot Password?
+          </a>
+          <button type="submit" className="login-button">
+            Sign in
+          </button>
+        </form>
+        <p className="signup-text">
+          Don't have an account?{" "}
+          <a href="#" className="signup-link">
+            Sign up
+          </a>
+        </p>
       </div>
     </div>
+
+    {/* Right Container */}
+    <div className="login-right">
+      <div className="login-image">
+        <img src={loginBanner} alt="Login Banner" />
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 };
 
